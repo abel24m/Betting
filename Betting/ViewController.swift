04ee15bfe44e30c80d2738543497cdcdd7470558
abel24m <abel24m@utexas.edu>
2020-09-07 22:52:22 -0500

    Initial Commit

---
+++ Betting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Betting
+//
+//  Created by Abel Moreno on 9/7/20.
+//  Copyright © 2020 Abel Moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
